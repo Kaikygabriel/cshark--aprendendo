b12f17ed3bf8b27a41f84670b955842e580cf67f
Kaiky gabriel alves <kaikygabrielalves707@gmail.com>
2025-05-03 08:54:09 -0300

    att

---
+++ ExercicioSaque/ExercicioSaque/Entity/Conta.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExercicioSaque.Entity.Exception;
+
+namespace ExercicioSaque.Entity
+{
+    internal sealed class Conta
+    {
+        public Conta() { }
+
+        public Conta(string nome, int id, double saldo, double saldoLimite)
+        {
+            if(saldo < 0)
+            {
+                throw new DominioException("Saldo tem que ser maior que zero");
+            }
+            Saldo = saldo;
+            Nome = nome;
+            Id = id;
+            SaldoLimite = saldoLimite;
+        }
+
+        public string Nome { get; private set; }
+        public  int Id{ get;private set; }
+        public  double Saldo { get; private set; }
+        public double SaldoLimite { get; private set; }
+
+        public void Sacar(double valor)
+        {
+            if(valor > SaldoLimite || valor > Saldo)
+            {
+                throw new DominioException("Valor a sacar e maior do que o limite da conta ou maior que o saldo da conta");
+            }
+            if (valor < 0)
+            {
+                throw new DominioException("Valor tem que ser maior que zero");
+            }
+            Saldo -= valor;
+            Console.WriteLine($"Saldo atual é R${Saldo.ToString("F2",CultureInfo.InvariantCulture)}");
+        }
+
+    }
+}
---
+++ ExercicioSaque/ExercicioSaque/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using ExercicioSaque.Entity;
+
+namespace ExercicioSaque
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                Console.Write("Nome : ");
+                string nome = Console.ReadLine();
+                Console.Write("Id :");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Saldo inicial :");
+                double saldo = double.Parse(Console.ReadLine());
+                Console.Write("LImite :");
+                double saldoLimite = double.Parse(Console.ReadLine());
+                Conta conta = new Conta(nome,id,saldo,saldoLimite);
+                Console.WriteLine("");
+                Console.Write("escreva o valor a sacar");
+                double valor = double.Parse(Console.ReadLine());
+                conta.Sacar(valor);
+                Console.WriteLine("Se deu error não vai aparecer");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR: " + ex.Message);
+            }
+        }
+    }
+}
