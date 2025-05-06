439b980561a2257496f181e22fb08fe139522f9e
Kaiky gabriel alves <kaikygabrielalves707@gmail.com>
2025-05-06 13:14:24 -0300

    Primeiro commit

---
+++ RevendoFIle/RevendoFIle/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace RevendoFIle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string Caminho = @"C:\Users\kaiky\Downloads\kaka\teste.txt";
+                string Caminho2 = @"C:\Users\kaiky\Downloads\kaka\teste2.txt";
+                FileInfo fb = new FileInfo(Caminho);
+                Console.WriteLine(fb.Length);
+                string[] linhas = File.ReadAllLines(fb.ToString());
+                foreach(string l in linhas)
+                {
+                    Console.WriteLine(l);
+                    Console.WriteLine(1);
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("ERROR : " + ex.Message);
+            }
+        }
+    }
+}
