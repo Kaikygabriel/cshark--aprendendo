76a8b7b635ccff3ccc18a8c6cda91b52def1b97d
Kaiky gabriel alves <kaikygabrielalves707@gmail.com>
2025-05-11 15:58:11 -0300

    Atualização Alocacao2

--- Alocacao2/Alocacao2/Entity/CarRental.cs
+++ Alocacao2/Alocacao2/Entity/CarRental.cs
@@ -17,9 +17,9 @@ namespace Alocacao2.Entity
             Model = model;
         }
 
-        public DateTime Entrada { get; private set; }
-        public DateTime Entregua { get; private set; }
-        public Veiculo Model { get; private set; } 
+        public DateTime Entrada { get; private set;}
+        public DateTime Entregua { get; private set;}
+        public Veiculo Model { get; private set;} 
         public Invoice Invoice { get;  set; }
     }
 }
