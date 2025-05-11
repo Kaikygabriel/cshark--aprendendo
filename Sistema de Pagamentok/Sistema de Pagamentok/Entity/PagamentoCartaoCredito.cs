9005a4b8ca0ee66f38435c746c9a410c47b7c856
Kaiky gabriel alves <kaikygabrielalves707@gmail.com>
2025-05-11 10:51:08 -0300

    att

---
+++ Sistema de Pagamentok/Sistema de Pagamentok/Entity/PagamentoCartaoCredito.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sistema_de_Pagamentok.Service;
+
+namespace Sistema_de_Pagamentok.Entity
+{
+    internal class PagamentoCartaoCredito : Pagamento
+    {
+        PagamentoCartaoCredito() { }
+
+        public PagamentoCartaoCredito(ITaxa taxa, double credito, double limite)
+        {
+            _taxa = taxa;
+            Credito = credito;
+            Limite = limite;
+        }
+
+        private ITaxa _taxa;
+        public double Credito { get; private set; } = 1000;
+        public double Limite { get; private set; }
+        public double ValorMensal{ get; private set; }
+        public void ProcessarPagamento(double valor)
+        {
+            double tax = _taxa.Taxa(valor);
+            Credito -= valor;
+            ValorMensal += valor + tax;
+        }
+    }
+}
---
+++ Sistema de Pagamentok/Sistema de Pagamentok/Service/TaxaCartao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema_de_Pagamentok.Service
+{
+    internal class TaxaCartao : ITaxa
+    {
+        public double Taxa(double valor) 
+        {
+            return valor *0.05;
+        }
+    }
+}
