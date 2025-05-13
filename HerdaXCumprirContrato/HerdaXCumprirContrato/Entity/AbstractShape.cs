d8d14eebc75227e6a04a13ebb8cec1abc66900d4
Kaiky gabriel alves <kaikygabrielalves707@gmail.com>
2025-05-13 17:32:33 -0300

    att

---
+++ HerdaXCumprirContrato/HerdaXCumprirContrato/Entity/AbstractShape.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using HerdaXCumprirContrato.Entity.Enum;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerdaXCumprirContrato.Entity
+{
+    internal abstract class AbstractShape : shape
+    {
+        public Color Color{ get; private protected set; }
+        public abstract double Area();   
+    }
+}
