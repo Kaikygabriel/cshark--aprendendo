b515be0d4c516b67ef010bafb750351b674f07a5
Kaiky gabriel alves <kaikygabrielalves707@gmail.com>
2025-05-15 10:09:40 -0300

    att

---
+++ Icontable/Icontable/Entity/Funcionario.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Icontable.Entity
+{
+    internal class Funcionario : IComparable
+    {
+        public Funcionario() { }
+        public string Nome { get; private set; }
+        public double Salario { get;private set; }
+        public Funcionario(string employe)
+        {
+            string[] vect = employe.Split(',');
+            Nome = vect[0];
+            Salario = double.Parse(vect[1]);
+        }
+        public override string ToString()
+        {
+            return $"{Nome} , R${Salario.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+
+        public int CompareTo(object? obj)
+        {
+            if(!(obj is Funcionario))
+            {
+                throw new Exception("ERROR não e do tipo funcionario");
+            }
+            Funcionario funcionario = (Funcionario)obj;
+            return Nome.CompareTo(funcionario.Nome);
+        }
+    }
+}
---
+++ Icontable/Icontable/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using Icontable.Entity;
+
+namespace Icontable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\kaiky\Downloads\Funcionarios.csv";
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<Funcionario> funcionario = new List<Funcionario>();
+                    while (!sr.EndOfStream)
+                    {
+                        funcionario.Add(new Funcionario(sr.ReadLine()));
+                    }
+                    funcionario.Sort();
+                    foreach (var f in funcionario)
+                    {
+                        Console.WriteLine(f);
+                    }
+                }
+            }
+            catch (IOException e) 
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
