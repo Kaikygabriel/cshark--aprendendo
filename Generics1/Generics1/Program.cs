72a63c72fdf4ea9f1926bbabbeb70faab1764385
Kaiky gabriel alves <kaikygabrielalves707@gmail.com>
2025-05-16 16:42:23 -0300

    att

---
+++ Generics1/Generics1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Generics1.Service;
+
+namespace Generics1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintService<int> p = new PrintService<int>();
+            Console.WriteLine("Quantos numeros");
+            int quantidade = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= quantidade; i++)
+            {
+                int Valor = int.Parse(Console.ReadLine());
+                p.AddValue(Valor);
+            }
+            Console.WriteLine("First :  " + p.First());
+            p.Print();
+        }
+    }
+}
---
+++ Generics1/Generics1/Service/PrintService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics1.Service
+{
+    internal sealed class PrintService<T>
+    {
+        private T[] _values = new T[10];
+        private int _count = 0;
+        public void AddValue(T value)
+        {
+            if(_count > 10)
+            {
+                throw new Exception("error");
+            }
+            _values[_count] = value;
+            _count ++;
+        }
+        public T First()
+        {
+            if (_count == 10)
+            {
+                throw new Exception("error");
+            }
+            return _values[0];
+        }
+        public void Print()
+        {
+           for(int i =0;i <= _count - 1; i++)
+            {
+                Console.WriteLine(_values[i]);
+            }
+        }
+    }
+}
