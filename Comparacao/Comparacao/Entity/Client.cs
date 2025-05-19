c996d071246f369b081ee6c7e870d97843475499
Kaiky gabriel alves <kaikygabrielalves707@gmail.com>
2025-05-19 16:49:55 -0300

    att

---
+++ Comparacao/Comparacao/Entity/Client.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comparacao.Entity
+{
+    internal sealed class Client 
+    {
+        public string Nome { get; internal set; }
+        public string Email{ get; internal set; }
+
+        
+        public override bool Equals(object? obj)
+        {
+            if (!(obj is Client))
+                throw new ArgumentException("Error tipo não igual a cliente");
+
+            Client cliente = (Client)obj;
+            return Email.Equals(cliente.Email);
+        }
+
+        public override int GetHashCode()
+        {
+            return Email.GetHashCode();
+        }
+    }
+}
+
---
+++ Comparacao/Comparacao/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Comparacao.Entity;
+
+namespace Comparacao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Client c = new Client() { Email = "Kaiky@gmail", Nome = "Kaiky" };
+            Client d = new Client() { Email = "Kaiky@gmail", Nome = "Gabriel" };
+            Console.WriteLine(c.Equals(d));
+            Console.WriteLine(c.GetHashCode());
+            Console.WriteLine(d.GetHashCode());
+            Console.WriteLine(d.GetHashCode().Equals(c.GetHashCode()));
+        }
+    }
+}
