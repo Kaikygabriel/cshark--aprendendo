748c129b73e1649e13024cb41b750cfb7746c6a2
Kaiky gabriel alves <kaikygabrielalves707@gmail.com>
2025-05-23 16:48:16 -0300

    att Directory list

---
+++ CourseDictonary/CourseDictonary/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CourseDictonary
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Dictionary<string,int>Deputados = new Dictionary<string,int>();
+            using (StreamReader rd = new StreamReader(@"C:\Users\kaiky\Downloads\Deputados.txt"))
+            {
+                while (!(rd.EndOfStream))
+                {
+                    string[] line = rd.ReadLine().Split(',');
+                    if (Deputados.ContainsKey(line[0]))
+                    {
+                        Deputados[line[0]] += int.Parse(line[1]);
+                    }
+                    else
+                    {
+                        Deputados.Add(line[0], int.Parse(line[1]));
+                    }
+                }
+            }
+            foreach(var a in Deputados)
+            {
+                Console.WriteLine($"{a.Key} : {a.Value}");
+            }
+        }
+    }
+}
